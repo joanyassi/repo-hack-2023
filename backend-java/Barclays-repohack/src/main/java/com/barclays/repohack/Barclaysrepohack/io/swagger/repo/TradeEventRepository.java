828c82900a03f42c49da31602fc1b7450661cb97
srnarra <srinath.narra@gmail.com>
2023-09-26 16:01:51 +0100

    Updated few updates

--- backend-java/Barclays-repohack/src/main/java/com/barclays/repohack/Barclaysrepohack/io/swagger/repo/TradeEventRepository.java
+++ backend-java/Barclays-repohack/src/main/java/com/barclays/repohack/Barclaysrepohack/io/swagger/repo/TradeEventRepository.java
@@ -3,6 +3,10 @@ package com.barclays.repohack.Barclaysrepohack.io.swagger.repo;
 import com.barclays.repohack.Barclaysrepohack.io.swagger.entities.TradeEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TradeEventRepository extends JpaRepository<TradeEvent, Long> {
+
+    List<TradeEvent> findByTradeId(Long tradeId);
 
 }
